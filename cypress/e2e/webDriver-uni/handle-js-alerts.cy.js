f237dd7408bee66824183a44e817c1da543661f8
hugohhernandez76 <hugohhernandez76@gmail.com>
2022-10-27 08:04:04 -0500

    Working with Iframe

--- cypress/e2e/webDriver-uni/handle-js-alerts.cy.js
+++ cypress/e2e/webDriver-uni/handle-js-alerts.cy.js
@@ -1,17 +1,56 @@
 /// <reference types="cypress" />
-describe('Handlinng Alerts', () => {
-    beforeEach(() => {
-        cy.visit("http://www.webdriveruniversity.com/");
-      });
-    it('Confirm Correct Text Alert', () => {
-        cy.get("#popup-alerts").invoke("removeAttr", "target").click()
-        cy.url().should("include", "Popup-Alerts")
-        //Validating I landed in the correct page so I extracted the text to confirm the page.
-        cy.xpath("//div[@id='main-header']/h1").then(($alertsPage) => {
-            const validateAlertsPage = $alertsPage.text();
-            expect(validateAlertsPage).to.eq("Annoying Popup & Alerts!")
-        })
-        cy.get("#button1").click()
-        
+describe("Handlinng Alerts", () => {
+  beforeEach(() => {
+    cy.visit("http://www.webdriveruniversity.com/");
+  });
+  it("Confirm Correct Text Alert", () => {
+    cy.get("#popup-alerts").invoke("removeAttr", "target").click();
+    cy.url().should("include", "Popup-Alerts");
+    //Validating I landed in the correct page so I extracted the text to confirm the page.
+    cy.xpath("//div[@id='main-header']/h1").then(($alertsPage) => {
+      const validateAlertsPage = $alertsPage.text();
+      expect(validateAlertsPage).to.eq("Annoying Popup & Alerts!");
     });
-})
+    cy.get("#button1").click();
+    //asserting text in the the alert box
+    cy.on("window:alert", (str) => {
+      expect(str).to.eq("I am an alert box!");
+    });
+  });
+
+  it("Confirm I have clicked the OK button", () => {
+    cy.get("#popup-alerts").invoke("removeAttr", "target").click();
+    cy.url().should("include", "Popup-Alerts");
+    //Validating I landed in the correct page so I extracted the text to confirm the page.
+    cy.xpath("//div[@id='main-header']/h1").then(($alertsPage) => {
+      const validateAlertsPage = $alertsPage.text();
+      expect(validateAlertsPage).to.eq("Annoying Popup & Alerts!");
+    });
+    cy.get("#button4").click();
+    //asserting text in the the alert box
+    cy.on("window:confirm", (str) => {
+        //user true to select OK on the alert or user false to select cancel
+        return true; 
+    });
+    cy.get("#confirm-alert-text").contains("You pressed OK!")
+
+  });
+
+  it.only("Confirm I have clicked the Cancel button", () => {
+    cy.get("#popup-alerts").invoke("removeAttr", "target").click();
+    cy.url().should("include", "Popup-Alerts");
+    //Validating I landed in the correct page so I extracted the text to confirm the page.
+    cy.xpath("//div[@id='main-header']/h1").then(($alertsPage) => {
+      const validateAlertsPage = $alertsPage.text();
+      expect(validateAlertsPage).to.eq("Annoying Popup & Alerts!");
+    });
+    cy.get("#button4").click();
+    //using window:confirm allows you to either select or cancel
+    cy.on("window:confirm", (str) => {
+        //user true to select OK on the alert or user false to select cancel
+        return false; 
+    });
+    cy.get("#confirm-alert-text").contains("You pressed Cancel!")
+
+  });
+});
---
+++ cypress/e2e/webDriver-uni/working-with-iframes.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+describe("Handlinng Alerts", () => {
+    beforeEach(() => {
+      cy.visit("http://www.webdriveruniversity.com/");
+      cy.title("contains", "WebDriver | IFrame")
+    });
+    it("Confirm Correct Text Alert", () => {
+      cy.get("#iframe").invoke("removeAttr", "target").click();
+      cy.url().should("include", "IFrame");
+        //We are using an alias to get access to the body where the other elements are allocated
+      cy.get("#frame").then(($iframe) => {
+        const body = $iframe.contents().find("body")
+        cy.wrap(body).as("iframe")
+      })
+      cy.get("@iframe").find("#button-find-out-more").click()
+      cy.get("@iframe").find("#myModal").as("modal")
+      cy.get("@modal").should(($expectedText) => {
+        const text = $expectedText.text()
+        expect(text).to.include("Welcome to webdriveruniversity.com we sell a wide range of electrical goods ")
+      })
+      cy.get("@modal").contains("Close").click()
+      
+    });
+});
