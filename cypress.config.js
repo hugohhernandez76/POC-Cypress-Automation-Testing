16596025305e979ae36d235716fb14936ee4812b
hugohhernandez76 <hugohhernandez76@gmail.com>
2022-11-08 11:13:04 -0600

    today's smoke test

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'fbtgi5',
+  projectId: "fbtgi5",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     baseUrl: "http://www.webdriveruniversity.com/",
@@ -15,14 +14,19 @@ module.exports = defineConfig({
     pageLoadTimeout: 120000,
     viewportHeight: 1000,
     viewportWidth: 1000,
+    video: false,
     screenshotOnRunFailure: true,
     trashAssetsBeforeRuns: true,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json",
+    },
 
-    env:{
+    env: {
       DEV: "http://www.webdriveruniversity.com",
       QA: "https://automationteststore.com/",
       STG: "Staging Environment",
-      PROD: "Production Environment"
-    }
+      PROD: "Production Environment",
+    },
   },
 });
